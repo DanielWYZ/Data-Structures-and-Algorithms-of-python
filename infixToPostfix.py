54c465b9c2b2c2f82bf2e4200b131ac6ac7e6ee5
Daniel <wangyz7@bupt.edu.cn>
2021-02-01 23:14:19 +0800

    2021.2.1 栈

---
+++ infixToPostfix.py
@@ -0,0 +1,33 @@
+from Stack import Stack
+
+
+def infixToPostfix(infixexpr):
+    # 记录操作符优先级
+    prec = {}
+    prec["*"] = 3
+    prec["/"] = 3
+    prec["+"] = 2
+    prec["-"] = 2
+    prec["("] = 1
+    # 解析表达式到单词列表
+    opStack = Stack()
+    postfixList = []
+    tokenList = infixexpr.split()
+
+    for token in tokenList:
+        if token in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" or token in "0123456789":
+            postfixList.append(token)
+        elif token == '(':
+            opStack.push(token)
+        elif token == ')':
+            topToken = opStack.pop()
+            while topToken != '(':
+                postfixList.append(topToken)
+                topToken = opStack.pop()
+        else:
+            while (not opStack.isEmpty()) and (prec[opStack.peek()] >= prec[token]):
+                postfixList.append(opStack.pop())
+            opStack.push(token)
+    while not opStack.isEmpty():
+        postfixList.append(opStack.pop())
+    return " ".join(postfixList)
