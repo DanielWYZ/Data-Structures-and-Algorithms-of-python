ae005e3ca98474d6fd60a974372b1af0f1a6e259
Daniel <wangyz7@bupt.edu.cn>
2021-02-02 12:00:17 +0800

    双端队列

---
+++ _Deque_/palchecker.py
@@ -0,0 +1,18 @@
+from _Deque_.Deque import Deque
+
+
+def palchecker(aString):
+    chardeque = Deque()
+    for ch in aString:
+        chardeque.addRear(ch)
+    stillEqual = True
+    while chardeque.size() > 1 and stillEqual:
+        first = chardeque.removeFront()
+        last = chardeque.removeRear()
+        if first != last:
+            stillEqual = False
+    return stillEqual
+
+
+print(palchecker("lsldsadw"))
+print(palchecker("abcddcba"))
